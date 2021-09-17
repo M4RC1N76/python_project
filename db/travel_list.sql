8f29789328da94f9f74e3b162113547b9462d5aa
Marcin Borowski <borowski1976@yahoo.com>
2021-09-17 14:27:17 +0100

    db tables added

---
+++ db/travel_list.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE countries(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    visited BOOLEAN
+);
+
+CREATE TABLE cities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    visited BOOLEAN
+);
